63e03e04f607a8a7cd9762b83fd9b615ca5b368f
brodyamcfarland <brodyamcfarland@gmail.com>
2023-02-18 18:50:19 -0800

    Added Currency Formatting and turned the basket preview part into a component

---
+++ components/CartPreview.tsx
@@ -0,0 +1,43 @@
+import { ShoppingCartIcon } from "@heroicons/react/24/outline";
+import { useSelector } from "react-redux";
+import type { RootState } from "../state/store";
+import CurrencyFormat from "react-currency-format";
+
+const CartPreview = () => {
+     const cartCount = useSelector(
+          (state: RootState) => state.cartCounter.value
+     );
+     return (
+          <div
+               className="flex p-2 items-center border-t border-gray-600 hover:bg-white/5 duration-300 hover:cursor-pointer"
+               title="Cart"
+          >
+               <div className="relative">
+                    <ShoppingCartIcon className="h-11 w-11" />
+                    <span className="absolute top-0 -right-1 h-5 w-5 bg-red-700 rounded-full flex items-center justify-center text-xs border shadow-md">
+                         {cartCount}
+                    </span>
+               </div>
+
+               <div className="flex flex-col text-xs px-2 text-right">
+                    <div className="flex">
+                         <p className="text-gray-400 w-10 pr-2">Items:</p>
+                         <span>{cartCount}</span>
+                    </div>
+
+                    <div className="flex text-right">
+                         <p className="text-gray-400 w-10 pr-2">Total:</p>
+                         <CurrencyFormat
+                              value={cartCount * 50}
+                              thousandSeparator={true}
+                              prefix={"$"}
+                              renderText={(value) => <>{value}</>}
+                              displayType={"text"}
+                         />
+                    </div>
+               </div>
+          </div>
+     );
+};
+
+export default CartPreview;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,6 +12,7 @@ import {
      TagIcon,
 } from "@heroicons/react/24/outline";
 import { useSession, signIn, signOut } from "next-auth/react";
+import CartPreview from "./CartPreview";
 
 interface Props {
      cartCount: number;
@@ -138,33 +139,7 @@ const Sidebar = ({ cartCount }: Props) => {
                          </div>
                     )}
 
-                    <div
-                         className="flex p-2 items-center border-t border-gray-600 hover:bg-white/5 duration-300 hover:cursor-pointer"
-                         title="Cart"
-                    >
-                         <div className="relative">
-                              <ShoppingCartIcon className="h-11 w-11" />
-                              <span className="absolute top-0 -right-1 h-5 w-5 bg-red-700 rounded-full flex items-center justify-center text-xs border shadow-md">
-                                   {cartCount}
-                              </span>
-                         </div>
-
-                         <div className="flex flex-col text-xs px-2 text-right">
-                              <div className="flex">
-                                   <p className="text-gray-400 w-10 pr-2">
-                                        Items:
-                                   </p>
-                                   <span>{cartCount}</span>
-                              </div>
-
-                              <div className="flex text-right">
-                                   <p className="text-gray-400 w-10 pr-2">
-                                        Total:
-                                   </p>
-                                   <span>$ {cartCount * 50}</span>
-                              </div>
-                         </div>
-                    </div>
+                    <CartPreview />
                </div>
           </header>
      );
